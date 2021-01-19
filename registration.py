bd369112b701c71e85433aec40ee4d7fc61d9f4b
TAVAKOLIClement <GhNosix*6tava>
2021-01-19 17:42:39 +0100

    updated all the first step registration functions

--- registration.py
+++ registration.py
@@ -12,7 +12,20 @@ import ratGistrationIO
 from skimage.measure import label, regionprops
 
 
-def apply_2d_rotation(vector, angle):
+def sum_list_of_vectors(list_of_vectors):
+    """
+    sums all the vectors of a list
+    :param list_of_vectors: input list
+    :return: resulting vector
+    """
+    final_vector = np.zeros(list_of_vectors[0].shape)
+    for vector in list_of_vectors:
+        final_vector += vector
+
+    return final_vector
+
+
+def apply_2d_rotation_to_a_vector(vector, angle):
     """
     just apply a 2d rotation to a vector depending on an input angle (anticlockwise)
     :param vector: 2d numpy vector
@@ -24,6 +37,113 @@ def apply_2d_rotation(vector, angle):
 
     return np.array([vector[0] * c - vector[1] * s,
                     vector[0] * s + vector[1] * c])
+
+
+def compute_2d_rotation(image, angle, interpolator_type="linear"):
+    """
+    computes a 2d rotation on an image based on an angle around axis z
+    :param image: input image
+    :param angle: angle
+    :param interpolator_type: type of interpolator (linear or nearest neighbor)
+    :return: rotated image
+    """
+    image_itk = Sitk.GetImageFromArray(image)
+    tx = Sitk.AffineTransform(image_itk.GetDimension())
+
+    c = math.cos(float(angle))
+    s = math.sin(float(angle))
+    tx.SetMatrix((c, s, 0,
+                  -s, c, 0,
+                  0, 0, 1))
+
+    tx.SetCenter((image.shape[2] / 2, image.shape[1] / 2, 0))
+
+    if interpolator_type == "linear":
+        interpolator = Sitk.sitkLinear
+    else:
+        interpolator = Sitk.sitkNearestNeighbor
+
+    image_itk = Sitk.Resample(image_itk, tx, interpolator, 0.0, image_itk.GetPixelIDValue())
+
+    resulting_image = Sitk.GetArrayFromImage(image_itk)
+
+    return resulting_image
+
+
+def calculate_rotation_matrix_between_3d_vectors(current_vector, target_vector):
+    """
+    computes the rotation matrix in order to align current_vector on target_vector (x, y, z)
+    :param current_vector: vector we're aligning
+    :param target_vector: vector we're aligning on
+    :return: rotation matrix
+    """
+
+    current_vector = current_vector / np.linalg.norm(current_vector)
+    target_vector = target_vector / np.linalg.norm(target_vector)
+
+    c = np.dot(current_vector, target_vector)  # [x, y, z]
+    l = np.cross(current_vector, target_vector)  # [x, y, z]
+    s = np.linalg.norm(l)
+
+    # Rotation matrix
+    kmat = np.array([[0, -l[2], l[1]], [l[2], 0, -l[0]], [-l[1], l[0], 0]])
+    matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+
+    return matrix
+
+
+def compute_3d_rotation(image, rotation_matrix, center_of_rotation, translation=[0, 0, 0]):
+    """
+    computes rotation arounda center_of_rotation absed on a rotation matrix
+    :param image: input image
+    :param rotation_matrix: rotation matrix
+    :param center_of_rotation: center of rotation (3D, [x, y, z]
+    :param translation: 3D translation in addition to the angular rotation
+    :return: rotated image
+    """
+    image_itk = Sitk.GetImageFromArray(image)
+
+    tx = Sitk.AffineTransform(image_itk.GetDimension())
+
+    tx.SetMatrix((rotation_matrix[0][0], rotation_matrix[1][0], rotation_matrix[2][0],
+                  rotation_matrix[0][1], rotation_matrix[1][1], rotation_matrix[2][1],
+                  rotation_matrix[0][2], rotation_matrix[1][2], rotation_matrix[2][2]))
+
+    tx.SetCenter((center_of_rotation[0], center_of_rotation[1], center_of_rotation[2]))  # [x, y, z]
+
+    tx.SetTranslation((translation[0], translation[1], translation[2]))
+
+    image_itk = Sitk.Resample(image_itk, tx, Sitk.sitkLinear, 0.0, image_itk.GetPixelIDValue())
+
+    image = Sitk.GetArrayFromImage(image_itk)
+
+    return np.copy(image)
+
+
+def retrieve_throat_centroid(mask):
+    """
+    Calculates the barycenter of a shape
+    :param mask: mask of the shape
+    :return: its barycenter
+    """
+    label_img = label(mask)
+    regions = regionprops(label_img)
+    centroid = regions[0].centroid
+    return centroid
+
+
+def count_the_needed_translation_for_black_slices(image):
+    """
+    looks at a rotated image in order to count the number of slices that become black.
+    :param image: input rotated image
+    :return: the number of slices that contain too much blackness
+    """
+    offset = 0
+    for slice_nb in range(0, image.shape[0]):
+        slice_of_image = image[slice_nb, :, :]
+        if len(slice_of_image[slice_of_image == 0]) > len(slice_of_image) * 0.4:
+            offset += 1
+    return offset
 
 
 def straight_triangle_rotation(image, skull, skull_bounding_box, baryctr_jaw_one, baryctr_jaw_two):
@@ -49,67 +169,30 @@ def straight_triangle_rotation(image, skull, skull_bounding_box, baryctr_jaw_one
     else:
         angle = math.acos(np.dot(target_vector, jaws_vector)/(np.linalg.norm(jaws_vector)*np.linalg.norm(target_vector)))
 
-    c = math.cos(float(angle))
-    s = math.sin(float(angle))
-
     skull_to_center_vector = np.array([skull_center[0] - image.shape[2]/2,
                                        skull_center[1] - image.shape[1]/2])
-    new_skull_to_center_vector = apply_2d_rotation(skull_to_center_vector, angle)
+    new_skull_to_center_vector = apply_2d_rotation_to_a_vector(skull_to_center_vector, angle)
 
     jaw_one_to_center_vector = np.array([baryctr_jaw_one[0] - image.shape[2] / 2,
                                          baryctr_jaw_one[1] - image.shape[1] / 2])
-    new_jaw_one_to_center_vector = apply_2d_rotation(jaw_one_to_center_vector, angle)
+    new_jaw_one_to_center_vector = apply_2d_rotation_to_a_vector(jaw_one_to_center_vector, angle)
 
     if new_jaw_one_to_center_vector[1] < new_skull_to_center_vector[1]:
         angle += math.pi
-        c = -c
-        s = -s
 
-    image_itk = Sitk.GetImageFromArray(image)
-    skull_itk = Sitk.GetImageFromArray(skull)
-
-    tx = Sitk.AffineTransform(image_itk.GetDimension())
-    tx.SetMatrix((c, s, 0,
-                  -s, c, 0,
-                  0, 0, 1))
-    interpolator = Sitk.sitkLinear
-
-    tx.SetCenter((image.shape[2]/2, image.shape[1] / 2, 0))
-    image_itk = Sitk.Resample(image_itk, tx, interpolator, 0.0, image_itk.GetPixelIDValue())
-    skull_itk = Sitk.Resample(skull_itk, tx, interpolator, 0.0, skull_itk.GetPixelIDValue())
-
-    resulting_skull = Sitk.GetArrayFromImage(skull_itk)
-    resulting_image = Sitk.GetArrayFromImage(image_itk)
+    resulting_image = compute_2d_rotation(image, angle, "linear")
+    resulting_skull = compute_2d_rotation(skull, angle, "nearest")
 
     return np.copy(resulting_image), np.copy(resulting_skull), float(angle)
 
 
-def retrieve_throat_centroid(mask):
-    """
-    Calculates the barycenter of a shape
-    :param mask: mask of the shape
-    :return: its barycenter
-    """
-    label_img = label(mask)
-    regions = regionprops(label_img)
-    centroid = regions[0].centroid
-    return centroid
-
-
-def sum_list_of_vectors(list_of_vectors):
-    """
-    sums all the vectors of a list
-    :param list_of_vectors: input list
-    :return: resulting vector
-    """
-    final_vector = np.zeros(list_of_vectors[0].shape)
-    for vector in list_of_vectors:
-        final_vector += vector
-
-    return final_vector
-
-
 def straight_throat_rotation(image, throat_mask_img):
+    """
+    rotates the image based on the segmentation of the throat (so that is is aligned with a [0, 0, 1] vector
+    :param image: input image
+    :param throat_mask_img: input throat segmentation
+    :return: the aligned image, the rotation matrix and the center of rotation plus the offset (due to the rotation)
+    """
 
     centroid_list = []
     vectors_list = []
@@ -122,9 +205,9 @@ def straight_throat_rotation(image, throat_mask_img):
             centroid = retrieve_throat_centroid(throat_mask_img[nbSlice, :, :])
             centroid_list.append(centroid)
             if nbSlice > 0:
-                vectors_list.append(np.array([centroid_list[-1][0] - centroid_list[-2][0],
-                                              centroid_list[-1][1] - centroid_list[-2][1],
-                                              z_length]))
+                vectors_list.append(np.array([z_length,
+                                              centroid_list[-1][0] - centroid_list[-2][0],
+                                              centroid_list[-1][1] - centroid_list[-2][1]]))# vector : [z, y, x]
             z_length = 0
 
     total_vector = sum_list_of_vectors(vectors_list)
@@ -147,45 +230,19 @@ def straight_throat_rotation(image, throat_mask_img):
         normalized_total_vector = total_vector/np.linalg.norm(total_vector)
         normalized_current_vector = vectors_list[-1] / np.linalg.norm(vectors_list[-1])
 
-    #normalized_total_vector_itk = np.copy(normalized_total_vector)
-    #normalized_total_vector_itk[1] = normalized_total_vector[0]
-    #normalized_total_vector_itk[0] = normalized_total_vector[1]
-
+    normalized_total_vector_itk = np.flip(np.copy(normalized_total_vector))
     print("Normalized total vector: ", normalized_total_vector)
-    print("Flipped vector :", normalized_total_vector)
+    print("Flipped vector :", normalized_total_vector_itk)
 
-    center_of_rotation = [centroid_list[0][0], centroid_list[0][1]]
+    center_of_rotation = [centroid_list[0][1], centroid_list[0][0], 0] # [x, y]
 
-    image_itk = Sitk.GetImageFromArray(image)
+    rotation_matrix = calculate_rotation_matrix_between_3d_vectors(normalized_total_vector_itk, np.array([0, 0, 1]))
 
-    c = np.dot(normalized_total_vector, np.array([0, 0, 1]))
-    s = np.linalg.norm(np.cross(normalized_total_vector, np.array([0, 0, 1])))
-    u = normalized_total_vector[0]
-    v = normalized_total_vector[1]
-    w = normalized_total_vector[2]
+    test_image = compute_3d_rotation(image, rotation_matrix, center_of_rotation, [0, 0, 0])
+    offset = count_the_needed_translation_for_black_slices(test_image)
 
-    # Matrix calculation
-    matrix = [[u * u * (1 - c) + c, u * v * (1 - c) - w * s, u * w * (1 - c) + v * s],
-              [u * v * (1 - c) + w * s, v * v * (1 - c) + c, v * w * (1 - c) - u * s],
-              [u * w * (1 - c) - v * s, v * w * (1 - c) + u * s, w * w * (1 - c) + c]]
-
-    tx = Sitk.AffineTransform(image_itk.GetDimension())
-
-    tx.SetMatrix((matrix[0][0], matrix[0][1], matrix[0][2],
-                  matrix[1][0], matrix[1][1], matrix[1][2],
-                  matrix[2][0], matrix[2][1], matrix[2][2]))
-
-    print("Center :", center_of_rotation[0], center_of_rotation[1], 0)
-    tx.SetCenter((0, center_of_rotation[1], center_of_rotation[0]))
-    interpolator = Sitk.sitkLinear
-
-    # FINDERREALSEARCH
-    tx.SetTranslation((0, 0, 100))
-
-    image_itk = Sitk.Resample(image_itk, tx, interpolator, 0.0, image_itk.GetPixelIDValue())
-
-    image = Sitk.GetArrayFromImage(image_itk)
-    return np.copy(image), center_of_rotation, u, v
+    image = compute_3d_rotation(image, rotation_matrix, center_of_rotation, [0, 0, offset])
+    return np.copy(image), rotation_matrix, center_of_rotation, offset
 
 
 def symmetry_based_registration(image, skull, skull_bounding_box, throat_coordinates, number_of_iterations):
@@ -196,7 +253,7 @@ def symmetry_based_registration(image, skull, skull_bounding_box, throat_coordin
     :param skull_bounding_box: skull bounding box (2D)
     :param throat_coordinates: coordinates of the segmented throat
     :param number_of_iterations: how many angles we're trying to check (0.5 degrees step)
-    :return: rotated image, calculated angle
+    :return: rotated image, rotated_skull
     """
 
     # The bounding box needs to be centered on the throat coordinates
@@ -212,28 +269,13 @@ def symmetry_based_registration(image, skull, skull_bounding_box, throat_coordin
     correct_angle = 0
     for i in range(0, number_of_iterations*2):
 
-        angle = -number_of_iterations + i
-
-        c = math.cos(float(angle) / 180 * math.pi)
-        s = math.sin(float(angle) / 180 * math.pi)
+        angle = float(-number_of_iterations + i) / 180 * math.pi
 
         image_copy = np.copy(image)
         skull_copy = np.copy(skull)
 
-        image_itk = Sitk.GetImageFromArray(image_copy)
-        skull_itk = Sitk.GetImageFromArray(skull_copy)
-
-        tx = Sitk.AffineTransform(image_itk.GetDimension())
-        tx.SetMatrix((c, s, 0,
-                      -s, c, 0,
-                      0, 0, 1))
-
-        tx.SetCenter((throat_coordinates[1], throat_coordinates[0], 0))
-        image_itk = Sitk.Resample(image_itk, tx, Sitk.sitkLinear, 0.0, image_itk.GetPixelIDValue())
-        skull_itk = Sitk.Resample(skull_itk, tx, Sitk.sitkNearestNeighbor, 0.0, skull_itk.GetPixelIDValue())
-
-        resulting_image = Sitk.GetArrayFromImage(image_itk)
-        resulting_skull = Sitk.GetArrayFromImage(skull_itk)
+        resulting_image = compute_2d_rotation(image_copy, angle, "linear")
+        resulting_skull = compute_2d_rotation(skull_copy, angle, "nearest")
 
         cropped_image = resulting_image[:, skull_bounding_box[2]:skull_bounding_box[3] + 1, skull_bounding_box[0]:skull_bounding_box[1] + 1]
         flipped_image = cropped_image[:, :, ::-1]
@@ -257,11 +299,12 @@ def symmetry_based_registration(image, skull, skull_bounding_box, throat_coordin
 
         normalized_value = count / len(number_of_zeros[number_of_zeros == 0])
 
-
         cross_correlation_list.append(normalized_value)
+
         if diff > normalized_value:
             correct_angle = angle
             diff = normalized_value
+
         print("angle : ", angle, "; metric :", normalized_value)
         increment += 1
 
@@ -270,24 +313,13 @@ def symmetry_based_registration(image, skull, skull_bounding_box, throat_coordin
 
     print("Best angle :", correct_angle)
 
-    c = math.cos(float(correct_angle) / 180 * math.pi)
-    s = math.sin(float(correct_angle) / 180 * math.pi)
-
-    image_itk = Sitk.GetImageFromArray(image_copy)
-    skull_itk = Sitk.GetImageFromArray(skull_copy)
-
-    tx = Sitk.AffineTransform(image_itk.GetDimension())
-    tx.SetMatrix((c, s, 0,
-                  -s, c, 0,
-                  0, 0, 1))
-    tx.SetCenter((throat_coordinates[1], throat_coordinates[0], 0))
-    image_itk = Sitk.Resample(image_itk, tx, Sitk.sitkLinear, 0.0, image_itk.GetPixelIDValue())
-    skull_itk = Sitk.Resample(skull_itk, tx, Sitk.sitkNearestNeighbor, 0.0, skull_itk.GetPixelIDValue())
-    resulting_image = Sitk.GetArrayFromImage(image_itk)
+    resulting_image = compute_2d_rotation(image_copy, correct_angle, "linear")
+    resulting_skull = compute_2d_rotation(skull_copy, correct_angle, "nearest")
 
     cropped_image = resulting_image[:, skull_bounding_box[2]:skull_bounding_box[3] + 1, skull_bounding_box[0]:skull_bounding_box[1] + 1]
+    cropped_skull = resulting_skull[:, skull_bounding_box[2]:skull_bounding_box[3] + 1, skull_bounding_box[0]:skull_bounding_box[1] + 1]
 
-    return np.copy(cropped_image), correct_angle
+    return np.copy(cropped_image), np.copy(cropped_skull), correct_angle
 
 
 def command_iteration(method):
@@ -301,7 +333,7 @@ def command_iteration(method):
     # print("Position = ", method.GetOptimizerPosition())
 
 
-def registration_computation_with_mask(moving_image, reference_image, moving_mask, reference_mask, is_rotation_needed = False, verbose = False):
+def registration_computation_with_mask(moving_image, reference_image, moving_mask, reference_mask, is_rotation_needed=False, verbose=False):
     """
     registration calculation based on a mask
     :param moving_image: image to register
@@ -410,45 +442,31 @@ def registration_computation_with_mask(moving_image, reference_image, moving_mas
     return calculated_translation_transformation
 
 
+def apply_rotation_pipeline(image, lcl_triangle_angle, rotation_matrix, lcl_throat_coordinates, offset, symmetry_angle):
+    """
+    applies all the rotations with the previously calculated angles/rotation matrix in order to align the rat with
+    the z axis
+    :param offset:
+    :param image: input image
+    :param lcl_triangle_angle: first function angle (in rad)
+    :param rotation_matrix: second function rotation matrix
+    :param lcl_throat_coordinates: will be used as the center of rotation for the rotation matrix
+    :param symmetry_angle: third function angle (in rad)
+    :return: rotated image
+    """
+    image = compute_2d_rotation(image, lcl_triangle_angle, "linear")
+    image = compute_3d_rotation(image, rotation_matrix, lcl_throat_coordinates, [0, 0, offset])
+    image = compute_2d_rotation(image, symmetry_angle, "linear")
+
+    return image
+
+import os
 import PyIPSDK
-import segmentation, resampling
+import segmentation, resampling, ratGistrationIO
 
 
 if __name__ == "__main__":
 
     folder_name = "C:\\Users\\ctavakol\\Desktop\\test_for_ratGistration\\R0873_13_AuC_PBS\\Above_Acquisition\\"
-    list_of_files = ratGistrationIO.create_list_of_files(folder_name, 'tif')
-    image_test = resampling.bin_resize(ratGistrationIO.open_seq(list_of_files), 2)
-
-    img_ipsdk = PyIPSDK.fromArray(image_test)
-
-    threshold_value = segmentation.find_threshold_value("Au")
-    print("Threshold Value :", threshold_value)
-
-    # -- Threshold computation
-    thresholded_img_ipsdk = Bin.thresholdImg(img_ipsdk, threshold_value, 3)
-
-    # -- Extracting skull
-    above_skull, skull_bbox, \
-    barycenter_jaw_one, barycenter_jaw_two, \
-    y_max_jaw_one, y_max_jaw_two = segmentation.extract_skull_and_jaws(thresholded_img_ipsdk)
-    ratGistrationIO.save_tif_sequence(above_skull, "C:\\Users\\ctavakol\\Desktop\\test_for_ratGistration\\R0873_13_AuC_PBS\\Skull_result\\")
-
-    straightened_image, above_skull, triangle_angle = straight_triangle_rotation(np.copy(image_test), above_skull,
-                                                                                 skull_bbox, barycenter_jaw_one,
-                                                                                 barycenter_jaw_two)
-    ratGistrationIO.save_tif_sequence(straightened_image, "C:\\Users\\ctavakol\\Desktop\\test_for_ratGistration\\R0873_13_AuC_PBS\\Straightened_image\\")
-
-    above_skull_ipsdk = PyIPSDK.fromArray(above_skull)
-    bbox = segmentation.skull_bounding_box_retriever(above_skull_ipsdk)
-
-    ##################### + WRITING VOLUME PART + #####################
-
-    # Cropping the volumes and the skull masks
-    throat_mask = segmentation.throat_segmentation(straightened_image, bbox, "Au")
-
-    ratGistrationIO.save_tif_sequence(throat_mask, "C:\\Users\\ctavakol\\Desktop\\test_for_ratGistration\\R0873_13_AuC_PBS\\Throat_mask\\")
-    # FORMANUAL1
-    straightened_image, throat_coordinates, u, v = straight_throat_rotation(straightened_image, throat_mask)
-
-    ratGistrationIO.save_tif_sequence(straightened_image, "C:\\Users\\ctavakol\\Desktop\\test_for_ratGistration\\R0873_13_AuC_PBS\\Straight_final_image\\")
+    l = folder_name.split("\\")
+    ratGistrationIO.remove_last_folder_in_path(folder_name)
